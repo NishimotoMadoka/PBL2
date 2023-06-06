a076dd19f2b0e46fae3aaeb9754c163e8e0ad448
Kishi Kanami <kd1311637@st.kobedenshi.ac.jp>
2023-06-06 11:09:11 +0900

    DBひな形つくったよ

--- DB/user_db.sql
+++ DB/user_db.sql
@@ -1,4 +1,16 @@
-#userテーブル
+# データベースPBL2の作成
+set names utf8;
+drop database if exists PBL2;
+create database PBL2 character set utf8 collate utf8_general_ci;
+
+# ユーザー'PBL2'に、パスワード'kobepbl2-f'を設定し、データベースPBL2に対するすべての権限を付与
+grant all privileges on PBL2.* to PBL2@localhost identified by 'kobepbl2-f';
+
+# データベースPBL2を使用する
+use PBL2;
+
+# テーブルusersの作成
+drop table if exists users; # 既にテーブルusersがあれば削除する
 CREATE TABLE users(
     user_id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(20) NOT NULL,
