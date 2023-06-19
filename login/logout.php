1304b44366fc82d79597c54292d0d75b8beacc03
Kadota123 <kd1300751@st.kobedenshi.ac.jp>
2023-06-19 09:50:17 +0900

    12

--- login/logout.php
+++ login/logout.php
@@ -11,7 +11,7 @@ setcookie('name', '', time() -  60 * 60 * 24 * 14, '/');
 setcookie('user_id', '', time() -  60 * 60 * 24 * 14, '/');
 
 header("Location:" . $login_php);
-<?php
+// <?php
 require_once __DIR__ . '/../header.php';
 require_once __DIR__ . '/../pre.php';
 
@@ -25,3 +25,4 @@ setcookie('name', '', time() -  60 * 60 * 24 * 14, '/');
 setcookie('user_id', '', time() -  60 * 60 * 24 * 14, '/');
 
 header("Location:" . $login_php);
+?>
