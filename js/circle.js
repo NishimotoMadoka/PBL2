1f694531df2684c75cb964164a9a61ddcb03fce0
Takakuwa Ryuki <kd1304094@st.kobedenshi.ac.jp>
2023-06-19 10:40:28 +0900

    scriptKBKB

--- js/circle.js
+++ js/circle.js
@@ -1,4 +1,6 @@
-let Time = [];//グラフの時刻
+//ここからsample2
+
+var Time = [];//グラフの時刻
 let Label = [];//項目名
 
 //円グラフ表示
@@ -20,7 +22,27 @@ window.onload = function () {
 
 //データ受け取り
 function post_LabelAndTime(){
-
   Time.push(document.getElementById("input_time").value);
   Label.push(document.getElementById("input_label").value);
+}
+
+
+//ここからsample3
+function drawChart(){
+  var ctx = document.getElementById('canvas').getContext('2d');
+  window.myChart = new Chart(ctx, {
+    type: 'bar',
+    data://ラベルと値
+     {
+        labels: Label,//ここにラベル
+        datasets: [{
+          Time
+        }],
+    },
+    options:{
+      legend:{
+        display: false,
+      }
+    }
+  });
 }
