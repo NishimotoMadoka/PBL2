3a09b24be7525846449a10c35f645b8da05d4f51
Kishi Kanami <kd1311637@st.kobedenshi.ac.jp>
2023-06-27 11:05:44 +0900

    topむり

--- top/toppage.php
+++ top/toppage.php
@@ -23,14 +23,17 @@ require_once __DIR__ . '/../pre.php';
       <?php
       $i=0;
       if(!empty($friends_users_id)){
+        
+        $friends_articles_array=array();
         foreach ($friends_users_id as $friend_user_id) {
             $friend_user_id=$friend_user_id['friend_user_id'];
             $friends_articles = $article->friendsArticles($friend_user_id);
+            
             foreach ($friends_articles as $friend_article) {
-              // var_dump($friend_article);
-              // $friends_article_array="";
+              
+              $friends_articles_array=array();
               // $friend_article.$i=$friend_article;
-              $friend_article_array=["user_id"=>$friend_article['user_id'],
+              $friend_article_array=array("user_id"=>$friend_article['user_id'] ,
                                         "article_id"=>$friend_article['article_id'],
                                         "post_date"=>$friend_article['post_date'],
                                         "start_time"=>$friend_article['start_time'],
@@ -38,27 +41,38 @@ require_once __DIR__ . '/../pre.php';
                                         "item_name"=>$friend_article['item_name'],
                                         "title"=>$friend_article['title'],
                                         "diary"=>$friend_article['diary']
-                                      ];
-                                      array_push($friends_articles_array,$friend_article_array);
+
+              
+            );
+
+            var_dump($friend_article_array);
+            $friends_articles_array= array_merge_recursive($friends_articles_array,$friend_article_array);
               // $friends_articles_array=array($friends_articles_array,$friend_article);
               // array_push($friends_article_array,$friend_article);
               // $friends_articles_array[]+=$friend_article;
                 // if(!empty($friends_articles)){
             }
         }
+       // var_dump($friends_articles_array);
       }
+      //var_dump($friends_articles_array);
       // array_multisort( array_map( "strtotime", array_column( $friends_articles_array, "A" ) ), SORT_ASC, $friends_articles_array ) ;
-      array_multisort(array_column($friends_articles_array,'post_date'),SORT_DESC,$friends_articles_array);
-      var_dump($friends_articles_array);
-      $sampleOutput = "";
-      for ( $indexA = 0; $indexA < is_countable( $friends_articles_array ); $indexA++ ) {
-        for ( $indexB = 0; $indexB < is_countable( $friends_articles_array[$indexA] ); $indexB++ ) {
-            $sampleOutput .= "{$friends_articles_array[$indexA][$indexB]},";
+     // array_multisort(array_column($friends_articles_array,'post_date'),SORT_DESC,$friends_articles_array);
+     array_multisort(array_map("strtotime", array_column($friends_articles_array,"post_date")),SORT_DESC,$friends_articles_array);
+     // $sampleOutput = "";
+       foreach($friends_articles_array as  $array){
+       foreach($array as $key => $value){
+         echo "key:{$key} | value:{$value}<br/>\n";
+        }
+       }
+      //for ( $indexA = 0; $indexA < is_countable( $friends_articles_array ); $indexA++ ) {
+       // for ( $indexB = 0; $indexB < is_countable( $friends_articles_array[$indexA] ); $indexB++ ) {
+            //$sampleOutput .= "{$friends_articles_array[$indexA][$indexB]},";
             // if ( $indexB == count( $friends_articles_array[$indexA] ) - 1  ) {
             //     $sampleOutput .= "<br />\n";
             // }
-        }
-      }
+        //}
+      //}
       ?>
       
         <!-- <a href="article/article_show.php?article_id=<?= $friend_article['article_id'] ?>">
