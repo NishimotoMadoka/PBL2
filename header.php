9690fffd4f638d86bc9692693ed90077e4244bbd
Totomi Takuto <kd1329046@st.kobedenshi.ac.jp>
2023-06-29 14:44:36 +0900

    header

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
 <?php
 require_once __DIR__ . '/pre.php';
 $url = $_SERVER['REQUEST_URI'];
-// if (($name == "no_login" && !strstr($url, 'login.php')) && ($name == "no_login" && !strstr($url, 'signup.php'))) {
-//   header("Location:$login_php");
-// }
+if (($name == "no_login" && !strstr($url, 'login.php')) && ($name == "no_login" && !strstr($url, 'signup.php'))) {
+  header("Location:$login_php");
+}
 ?>
 
 <!DOCTYPE html>
@@ -18,7 +18,8 @@ $url = $_SERVER['REQUEST_URI'];
 
 <body>
   <!-- header部分 -->
-  <header>
+  <header></header>
+  <header id="scrollArea">
   <div class="top-info">
       <a href="<?= $toppage_php ?>">
         <div class="logo_img"><img src=<?php echo $logo_img ?> alt="FACT"></div>
@@ -33,7 +34,7 @@ $url = $_SERVER['REQUEST_URI'];
             echo '<li class="nav-list-item"><a class="fa-solid fa-right-to-bracket" href="' . $login_php . '"> ログイン</a></li>';
           } else {
             echo '<li class="nav-list-item"><a class="fa-solid fa-address-card" href="' . $mypage_php . '"> マイページ</a></li>';
-            echo '<li class="nav-list-item"><a class="fa-solid fa-pen" href="' . $post_php . '"> 投稿</a></li>';
+            echo '<li class="nav-list-item"><a class="fa-solid fa-pen" href="' . $post_php . '"> 記事・作品</a></li>';
             echo '<li class="nav-list-item"><a class="fa-solid fa-right-from-bracket" href="' . $logout_php . '"> ログアウト</a></li>';
           }
           ?>
@@ -41,52 +42,22 @@ $url = $_SERVER['REQUEST_URI'];
       </ul>
     </nav>
   </header>
-  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
   <script>
-    var beforePos = 0;//スクロールの値の比較用の設定
-
-//スクロール途中でヘッダーが消え、上にスクロールすると復活する設定を関数にまとめる
-function ScrollAnime() {
-    var elemTop = $('#area-2').offset().top;//#area-2の位置まできたら
-  var scroll = $(window).scrollTop();
-    //ヘッダーの出し入れをする
-    if(scroll == beforePos) {
-    //IE11対策で処理を入れない
-    }else if(elemTop > scroll || 0 > scroll - beforePos){
-    //ヘッダーが上から出現する
-    $('#header').removeClass('UpMove'); //#headerにUpMoveというクラス名を除き
-    $('#header').addClass('DownMove');//#headerにDownMoveのクラス名を追加
-    }else {
-    //ヘッダーが上に消える
-        $('#header').removeClass('DownMove');//#headerにDownMoveというクラス名を除き
-    $('#header').addClass('UpMove');//#headerにUpMoveのクラス名を追加
+    var startPos = 0, winScrollTop = 0;
+// scrollイベントを設定
+window.addEventListener('scroll', function () {
+    winScrollTop = this.scrollY;
+    if (winScrollTop >= startPos) {
+        // 下にスクロールされた時
+        if (winScrollTop >= 200) {
+            // 下に200pxスクロールされたら隠す
+            document.getElementById('scrollArea').classList.add('hide');
+        }
+    } else {
+        // 上にスクロールされた時
+        document.getElementById('scrollArea').classList.remove('hide');
     }
-    
-    beforePos = scroll;//現在のスクロール値を比較用のbeforePosに格納
-}
-
-
-// 画面をスクロールをしたら動かしたい場合の記述
-$(window).scroll(function () {
-  ScrollAnime();//スクロール途中でヘッダーが消え、上にスクロールすると復活する関数を呼ぶ
-});
-
-// ページが読み込まれたらすぐに動かしたい場合の記述
-$(window).on('load', function () {
-  ScrollAnime();//スクロール途中でヘッダーが消え、上にスクロールすると復活する関数を呼ぶ
-});
-
-
-
-
-//リンク先のidまでスムーススクロール
-//※ページ内リンクを行わない場合は不必要なので削除してください
-    var headerH = $("#header").outerHeight(true);//headerの高さを取得    
-    $('#g-navi li a').click(function () {
-  var elmHash = $(this).attr('href'); 
-  var pos = $(elmHash).offset().top-headerH;//header分の高さを引いた高さまでスクロール
-  $('body,html').animate({scrollTop: pos}, 1000);
-  return false;
+    startPos = winScrollTop;
 });
   </script>
 </body>
