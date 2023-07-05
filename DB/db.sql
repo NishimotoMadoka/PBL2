994898bbc857845ce088e9e88e569ca7cd9bca55
Kishi Kanami <kd1311637@st.kobedenshi.ac.jp>
2023-07-06 00:47:23 +0900

    a

--- DB/db.sql
+++ DB/db.sql
@@ -29,6 +29,18 @@ CREATE TABLE users(
     )
 );
 
+# テーブルusersへデータを入力
+
+INSERT INTO `users`(`user_id`, `name`, `mail`, `profile_comment`, `friend_code`, `icon`, `password`)
+ VALUES ('1','まどちゃん','madokan@iclound.com','よろしくおねがいします','6tJQOTsvY','madokapush2.gif','madoka1125');
+
+INSERT INTO `users`(`user_id`, `name`, `mail`, `profile_comment`, `friend_code`, `icon`, `password`)
+ VALUES ('2','かなみ','kanakana@st.kobedenshi.ac.jp','よろしくおねがいします','7HKmZUp6i','kanakanapaper20230605.jpg','kanami');
+
+INSERT INTO `users`(`user_id`, `name`, `mail`, `profile_comment`, `friend_code`, `icon`, `password`)
+ VALUES ('3','たくちゃん','takuchandayo@icloud.com','たくちゃんだヨ','RdzLvkrHD','takuchandayo27893617_1828729520479480_203621153796...','takuchan');
+
+
 #テーブルdiary_listの作成
 drop table if exists diary_list; # 既にテーブルdiary_listがあれば削除する
 CREATE TABLE diary_list(
@@ -44,6 +56,15 @@ CREATE TABLE diary_list(
     ),
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
+
+ # テーブルdiary_listへデータを入力
+INSERT INTO `diary_list`(`article_id`, `user_id`, `title`, `diary`, `diary_date`) 
+ VALUES ('1','1','岡先生について','岡先生はなんでBばっか見るんですか。','2023-06-01');
+
+INSERT INTO `diary_list`(`article_id`, `user_id`, `title`, `diary`, `diary_date`) 
+ VALUES ('2','3','式先生のスマートウォッチ','今日式先生にスマートウォッチを見してもらいました。
+フォッシルというやつでした。かっこよかったです...','2023-06-03');
+
 #テーブルarticle_listの作成
 drop table if exists article_list; # 既にテーブルarticle_listがあれば削除する
 CREATE TABLE article_list(
@@ -67,6 +88,19 @@ CREATE TABLE article_list(
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
+ # テーブルarticles_listへデータを入力
+INSERT INTO `article_list`(`article_id`, `user_id`, `start_time`, `end_time`, `item_name`,
+`good`, `post_date`, `article_image`, `article_public`)
+ VALUES ('1','1','00:00,08:00,08:15,,,,,,,,','08:00,08:15,08:40,,,,,,,,',
+ '睡眠,ごはん,準備,,,,,,,,','0','2023-06-01','madokan210615-480x280.jpg','0');
+
+
+INSERT INTO `article_list`(`article_id`, `user_id`, `start_time`, `end_time`, `item_name`,
+`good`, `post_date`, `article_image`, `article_public`)
+ VALUES ('2','3','01:00,09:00,10:00,,,,,,,,','09:00,10:00,11:30,,,,,,,,',
+ '睡眠,朝ごはん,筋トレ,,,,,,,,','0','2023-06-03','madokan210615-480x280.jpg','0');
+
+
 #テーブルfriends_listの作成
 drop table if exists friends_list; # 既にテーブルfriends_listがあれば削除する
 CREATE TABLE friends_list(
@@ -78,3 +112,12 @@ CREATE TABLE friends_list(
     )
     -- FOREIGN KEY (friend_user_id) REFERENCES users(user_id)
 );
+
+ # テーブルfriend_listへデータを入力
+INSERT INTO `friends_list`(`login_user_id`, `friend_user_id`) VALUES ('2','1');
+
+INSERT INTO `friends_list`(`login_user_id`, `friend_user_id`) VALUES ('2','3');
+
+INSERT INTO `friends_list`(`login_user_id`, `friend_user_id`) VALUES ('3','2');
+
+
