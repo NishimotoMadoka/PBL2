638bc8b5fdd6c0ae1a2ef7a5f82aa825a949da35
NishimotoMadoka <kd1321687@st.kobedenshi.ac.jp>
2023-10-16 10:23:32 +0900

    たくちゃんよう

--- top/info.php
+++ top/info.php
@@ -9,12 +9,14 @@ require_once __DIR__ . '/../header.php';
 <?php 
   $_start = $_POST['starttime'];//$_POST で受け取る
   $_end = $_POST['endtime'];
-  $_label =$_POST['item'];//DBから来た値
+  $_label = $_POST['item'];//DBから来た値
+  $_color = $_POST['color'];
 
     
   $_start = json_encode($_start);
   $_end = json_encode($_end);
   $_label = json_encode($_label);//phpからきた、値をjavascriptに変換
+  $_color = json_encode($_color);
 
 ?>
 <div style="width:100%">
@@ -32,6 +34,8 @@ require_once __DIR__ . '/../header.php';
 const sample1 = <?php echo $_start;?>;
 const sample2 = <?php echo $_end;?>;
 const sample3 = <?php echo $_label;?>;
+const color = <?php echo $_color;?>;
+
 console.log(sample1);
 console.log(sample2);
 console.log(sample3);
